eae45b1a78d82a6abf4a1958fafbbfa4c4422dd1
aryamadea <62591211+aryamadea@users.noreply.github.com>
2020-03-25 18:03:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,65 +1,117 @@
 <!DOCTYPE html>
+
 <html>
 
+
+
 <head>
+
   <style>
+
     table {
+
       font-family: arial, sans-serif;
+
       border-collapse: collapse;
+
       width: 100%;
+
     }
+
+
 
     td,
+
     th {
+
       border: 1px solid #dddddd;
+
       text-align: left;
+
       padding: 8px;
+
     }
+
+
 
     tr:nth-child(even) {
+
       background-color: #dddddd;
+
     }
+
   </style>
+
 </head>
+
 <?php
+
 require_once("database.php");
+
 ?>
+
 <body>
 
-  <h2>Daftar Nama Mahasiswa</h2>
+
+
+  <h2>Driver</h2>
+
+
 
   <table>
+
     <tr>
-      <th>ID</th>
+
+      <th>nrp</th>
+
       <th>Name</th>
+
       <th>License</th>
+
     </tr>
+
     <?php
+
     $sql = "SELECT * FROM student ORDER by name ASC";
+
     $result = $c->query($sql);
-    if ($result->num_rows > 0) 
-    {
-      $student = array();
+
+    if ($result->num_rows > 0) {
+
+
+
+      $drivers = array();
+
       $i = 0;
-      while ($obj = $result->fetch_assoc()) 
-      {
-       
-       
-       echo '<tr>';
+
+      while ($obj = $result->fetch_assoc()) {
+
+        echo '<tr>';
+
         echo '<td>' . $obj['nrp'] . '</td>';
+
         echo '<td>' . $obj['nama'] . '</td>';
+
         echo '<td>' . $obj['jurusan'] . '</td>';
-        echo'</tr>';
+
       }
-    } 
-    else 
-    {
-       echo "Empty table.";
+
+    } else {
+
+      echo "Empty table.";
+
       die();
+
     }
+
     ?>
+
   </table>
 
+
+
 </body>
+
+
 
 </html>
